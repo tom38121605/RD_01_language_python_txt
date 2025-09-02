2c64512441dd6a2c0b6840f9ecb62f35a7050a32
tom38121605 <13538121605@139.com>
2025-09-02 14:20:22 +0800

    111

---
+++ 示例02-uart-线程/uartsimple-normal-05-af-英文注释-ok-k.py
@@ -0,0 +1,54 @@
+﻿import serial
+import time
+import serial.tools.list_ports
+import threading
+import re
+
+
+text = "aa,bb;123"
+pattern = r"[;, ]"
+result = re.split(pattern, text)
+print("splited:", result)
+
+ports=serial.tools.list_ports.comports()
+for port, desc, hwid in sorted(ports):
+    # print(f"{port}: {desc} ({hwid})")
+    print(f"{port}: {desc} ")
+
+def receive_and_send_data(ser):
+    try:
+        while True:
+
+            if ser.in_waiting > 0:
+
+                received_data = ser.read(ser.in_waiting)
+                print(f"received data: {received_data}")
+
+                ser.write(received_data)
+
+    except serial.SerialException as e:
+        print(f"uart receive err: {e}")
+    except KeyboardInterrupt:
+        print("The uart rx thread is interrupted by the user")
+
+
+try:
+
+    ser = serial.Serial('COM17', 1000000, timeout=1)
+    print(f"Successfully connected: {ser.name}")
+
+    receive_thread = threading.Thread(target=receive_and_send_data, args=(ser,))
+    receive_thread.daemon = True
+    receive_thread.start()
+
+    while True:
+        pass
+
+except serial.SerialException as e:
+    print(f"uart connect err: {e}")
+except KeyboardInterrupt:
+    print("The program was interrupted by the user。")
+finally:
+    if 'ser' in locals() and ser.is_open:
+        ser.close()
+        print("uart connection closed")
